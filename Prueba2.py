9556ffd42676ba2c9821bffd67e50ddffddf93da
Chrystiam Felipe Gómez Upegui <chrystiamgomez@hotmail.com>
2022-06-17 05:37:22 -0500

    Pureba1 commit desde Fork

--- Prueba2.py
+++ Prueba2.py
@@ -8,3 +8,6 @@ Created on Fri Jun  3 07:23:32 2022
 def pruebafunc():
     return(print("Esta es una prueba de la función en Prueba2.py"))
 
+def funcion2():
+    return(print("Esta es la función que se imprimiría en la PruebaFork"))
+
